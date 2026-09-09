a45344ea8304354411a4bed426190a2e3fc9b366
Raphael <raphacgon@gmail.com>
2026-09-09 12:05:45 -0300

    Add '-y' flag to apt and flatpak commands & Cleanup Routines
    * Added -y flags to the commands to allow the script to run without asking permission to update or cleanup software.
    
    *Added Cleanup Routines with 'apt autoremove' e 'flatpak uninstall --unused'.

--- update.sh
+++ update.sh
@@ -1,16 +1,29 @@
+#!/bin/bash
+
 clear;
 echo 'Linux System Flatpak and Apt Updater Script';
 echo 'Developed by raphacgon'
 sleep 2;
+
 clear;
 sudo apt update;
 sleep 2;
+
 clear; 
 echo 'Sources updated. Updating system.';
 sleep 2; 
-sudo apt upgrade;
+
+sudo apt upgrade -y;
 sleep 2;
-sudo flatpak upgrade;
+
+sudo apt autoremove -y
 sleep 2;
+
+flatpak upgrade -y;
+sleep 2;
+
+flatpak uninstall --unused -y
+sleep 2;
+
 clear; 
 echo 'Your system is up to date.';
